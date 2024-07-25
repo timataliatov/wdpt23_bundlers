8c618380d821e79a2044ab60af3fdae2611082f5
timataliatov <ttaliatov@gmail.com>
2024-07-25 17:31:55 +0200

    gitignore update + maps

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const myLocations = [
-  {
-    name: 'Example Location 1',
-    location: [51.505, -0.09],
-    description: 'This is an example location'
+const myLocations = [{
+    name: 'WBS CODING SCHOOL',
+    location: [52.457131, 13.54007],
+    description: 'The best coding school in the world'
   },
   {
-    name: 'Example Location 2',
-    location: [51.51, -0.1],
-    description: 'This is another example location'
+    name: 'Alexanderplatz',
+    location: [52.521918, 13.413215],
+    description: 'The most famous square in Berlin'
+  },
+  {
+    name: 'Brandenburg Gate',
+    location: [52.516275, 13.377704],
+    description: 'The most famous gate in Berlin'
+  },
+  {
+    name: 'Berlin Wall',
+    location: [52.507541, 13.39032],
+    description: 'The most famous wall in Berlin'
   }
 ];
 
