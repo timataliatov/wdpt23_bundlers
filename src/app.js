03de8cea2c40af962629529f8b9cf6a2e88025d1
timataliatov <ttaliatov@gmail.com>
2024-07-25 18:11:33 +0200

    files organised ready to deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
-import Leaflet from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet'; // import everything from leaflet
+import 'leaflet/dist/leaflet.css'; // import leaflet css
 
-const myLocations = [{
+const WBS = [52.457131, 13.54007]; // WBS coordinates
+const map = Leaflet.map('map').setView(WBS, 13); // create a map object with a center and zoom level
+const markerIcon = Leaflet.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconAnchor: [10, 20]
+}); // There was an issue with the default marker icon, so we create a new one
+Leaflet.marker(WBS, { icon: markerIcon }).addTo(map); // add a marker to the map at the WBS coordinates
+
+Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map); // add a tile layer to the map, the tiles are those images that make up the map
+// My locations
+const myLocations = [
+  {
     name: 'WBS CODING SCHOOL',
     location: [52.457131, 13.54007],
     description: 'The best coding school in the world'
@@ -22,18 +36,13 @@ const myLocations = [{
     description: 'The most famous wall in Berlin'
   }
 ];
-
-const map = Leaflet.map('map').setView([51.505, -0.09], 13);
-
-Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
+// Add markers to the map with a popup
 myLocations.forEach(location => {
-  Leaflet.marker(location.location)
-    .addTo(map)
-    .bindPopup(location.description);
+  Leaflet.marker(location.location, { icon: markerIcon })
+    .bindPopup(location.description)
+    .addTo(map);
 });
 
+// Set the view to the bounds of all markers
 const bounds = Leaflet.latLngBounds(myLocations.map(location => location.location));
 map.fitBounds(bounds);
